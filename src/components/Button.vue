bf3b4e5704dcb729ce514c8ec674ec284be193c6
Mahdi Khavari <mkhavari813@gmail.com>
2022-02-07 00:46:33 +0330

    add Header and Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button>
+        {{title}}
+    </button>
+</template>
+<script>
+export default {
+    name : "Button",
+    props : {
+        title : String
+    }
+}
+</script>
+<style scoped>
+    button {
+        padding: 10px;
+        outline: none;
+        border: none;
+        background: gainsboro;
+        color: #242424;
+        font-size: 13px;
+        cursor: pointer;
+    }
+</style>
